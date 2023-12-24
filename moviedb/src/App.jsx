2999e566e69c5fe61bbc737ef32fd4a5ad0ba6ca
Shamaun-Nabi@ <dev.shamaun@gmail.com>
2023-12-24 10:17:52 +0600

    first commit

---
+++ moviedb/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <>
+      <Button className="mt-10 ml-24" color="primary">
+        Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
